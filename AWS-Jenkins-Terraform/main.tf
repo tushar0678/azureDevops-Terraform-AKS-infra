6fed27e45c2bd46a493a2b1a1bfde716925c47d3
Tushar Shukla <tusharshukla211@gmail.com>
2025-07-04 12:05:18 +0530

    aws code added in seperate repo

--- AWS-Jenkins-Terraform/main.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-tfstate-bucket"
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
-  }
-}
